c8a170cb2fab9700afb6498d094964779500ada4
ricardo <rcostaj00@gmail.com>
2022-03-12 20:22:14 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace classes
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        //constructor
+        public BankAccount (string name, decimal initialBalance)
+        {
+            this.Owner = name;
+
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            MakeDeposit(initialBalance, DateTime.Now, "Deposit Done");
+            MakeWithdrawal(initialBalance, DateTime.Now, "Withdrawal Done");
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var item in allTransactions)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Ricardo", 2000);
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+          
+        }
+    }
+
+}
+
+
+
+
+
+
---
+++ Transations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace classes
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction (decimal amount, DateTime Date, string notes )
+        {
+            this.Amount = amount;
+            this.Date = Date;
+            this.Notes = notes;
+        }
+    }
+}
